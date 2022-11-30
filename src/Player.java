54f8be3f9ef99b078dea736080112e6424284a1e
Ágoston Babicz <97826931+AgostonBabicz@users.noreply.github.com>
2022-11-30 09:57:07 +0100

    Comments for the Player class]

--- src/Player.java
+++ src/Player.java
@@ -9,33 +9,60 @@ public class Player
   private String studentID;
   private boolean membership;
 
+  /**
+   * The Constructor for the Player class
+   * */
   public Player(String name, String studentID)
   {
     this.name = name;
     this.studentID = studentID;
   }
 
+  /**
+   * Accessor method to get the name of the current player
+   * @return The name of the player
+   * */
   public String getName()
   {
     return name;
   }
+  /**
+   * Mutator method to change the name of the current player
+   * @param name The new name of the player
+   * */
   public void setName(String name)
   {
     this.name = name;
   }
+  /**
+   * Accessor method for the current player's studentID
+   * @return The player's studentID
+   * */
   public String getStudentID()
   {
     return studentID;
   }
+  /**
+   * Accessor method to get whether the current player is a member or not
+   * @return Whether the player pays membership or not
+   * */
   public boolean isMembership()
   {
     return membership;
   }
+  /**
+   * Mutator method to change the player's status based on the payment of the membership
+   * @param membership The new state of the player
+   * */
   public void setMembership(boolean membership)
   {
     this.membership = membership;
   }
 
+  /**
+   * Method to compare 2 Player objects
+   * @return Whether the Players are identical or not
+   * */
   public boolean equals(Object obj)
   {
     if (obj==null||obj.getClass()!=getClass())
