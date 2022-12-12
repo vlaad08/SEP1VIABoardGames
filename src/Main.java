f21717d811c11a491ecb2ea3d5e383b1ab306cd8
vlaad08 <62983956+vlaad08@users.noreply.github.com>
2022-12-12 15:01:34 +0100

    Revert "main"
    This reverts commit 88958a446981c1e62a5ccaff7133943531436773.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import FXML.ApplicationGUI;
 import javafx.application.Application;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    Application.launch(ApplicationGUI.class);
   }
 }
